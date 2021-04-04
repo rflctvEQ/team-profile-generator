a45e519c7fc08834b56077ecca0d69bdd7617b61
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-04-04 00:21:45 -0400

    added custom styling, created misc folder for my template html used for creating html template in script

--- src/template.js
+++ src/template.js
@@ -1,55 +1,55 @@
 // function that will generate all html elements specific to user's responses to inquirer questions
 const generateTeamMembers = function(teamList) {
-    console.log(teamList);
+    // console.log(teamList);
     const generateManager = function(manager) {
-        console.log(manager);
-        return `<div class="card col-4">
-        <div class="card-header">
-          <h2>${manager.getName()}</h2>
-          <h3>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: ${manager.getEmail()}</li>
-                <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
-              </ul>                        
-        </div>
-    </div>`
+        // console.log(manager);
+        return `<div class="card col-4 employeeCard">
+    <div class="card-header">
+        <h2>${manager.getName()}</h2>
+        <h3>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: ${manager.getEmail()}</li>
+            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>                        
+    </div>
+</div>`
     }
 
     const generateEngineer = function(engineer) {
-        console.log(engineer);
-        return `<div class="card col-4">
-        <div class="card-header">
-          <h2>${engineer.getName()}</h2>
-          <h3>${engineer.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${engineer.getId()}</li>
-                <li class="list-group-item">Email: ${engineer.getEmail()}</li>
-                <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
-              </ul>                        
-        </div>
-    </div>`
+        // console.log(engineer);
+        return `<div class="card col-4 employeeCard">
+    <div class="card-header">
+        <h2>${engineer.getName()}</h2>
+        <h3>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+            <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+            </ul>                        
+    </div>
+</div>`
     }
 
     const generateIntern = function(intern) {
-        console.log(intern);
-        return `<div class="card col-4">
-        <div class="card-header">
-          <h2>${intern.getName()}</h2>
-          <h3>${intern.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${intern.getId()}</li>
-                <li class="list-group-item">Email: ${intern.getEmail()}</li>
-                <li class="list-group-item">School: ${intern.getSchool()}</li>
-              </ul>                        
-        </div>
-    </div>`
+        // console.log(intern);
+        return `<div class="card col-4 employeeCard">
+    <div class="card-header">
+        <h2>${intern.getName()}</h2>
+        <h3>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: ${intern.getEmail()}</li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>                        
+    </div>
+</div>`
     }
 
     const renderedHtml = []; 
@@ -84,11 +84,12 @@ return `<!doctype html>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
+    <link href="./style.css" rel="stylesheet">
     <title>My Team</title>
   </head>
   <body>
     <header>
-        <h1 class="p-5 text-align-center">My Team</h1>
+        <h1 class="p-5 text-align-center"><span class="my">My</span> <span class="team">Team</span></h1>
     </header>
     <main class='container'>
         <div class="row">
